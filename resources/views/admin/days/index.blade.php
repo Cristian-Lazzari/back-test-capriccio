9dc879ec28cd095eca1b74cf5c232e88f2ed2741
DavideFarci <yank92.df@gmail.com>
2024-03-19 13:03:22 +0100

    griglia calendario

--- resources/views/admin/days/index.blade.php
+++ resources/views/admin/days/index.blade.php
@@ -2,10 +2,11 @@
 
 @section('contents')
 
-{{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
-<?php 
-$month_name=['gennaio','febbraio','marzo','aprile', 'maggio','giugno','luglio','agosto','settembre','ottobre','novrembre','dicembre']
- ?>
+    {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
+    <?php 
+        $month_name = ['gennaio','febbraio','marzo','aprile', 'maggio','giugno','luglio','agosto','settembre','ottobre','novrembre','dicembre'];
+        $days_week  = ["Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"];
+    ?>
 
     <h1 class="m-3">SCEGLI UN GIORNO</h1>    
 
@@ -13,6 +14,7 @@ $month_name=['gennaio','febbraio','marzo','aprile', 'maggio','giugno','luglio','
     <a href="{{ route('admin.months.index') }}" class="btn btn-dark my-3">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4z"/></svg>
     </a>
+
     <div class="flex d-5 ">
         @if ($month_id !== 1) 
             @if ($days[1]->m !== 1)
@@ -43,22 +45,44 @@ $month_name=['gennaio','febbraio','marzo','aprile', 'maggio','giugno','luglio','
 
     </div>
     
-        <table class="table table-striped">
+    <div class="container pt-3">
+        <div class="days_w">
+            @foreach ($days_week as $day_week)
+                <div class="day_w">
+                    {{ $day_week }}
+                </div>
+            @endforeach
+        </div>
+        <div class="day_grid">
+            @foreach ($days as $day)
+                <a 
+                    class="day"
+                    style="grid-column-start: {{ $day->day_w }} "
+                    href="{{ route('admin.days.show', ['day' => $day->id])  }}"
+                >
+                    {{$day->day}}
+                </a>
+            @endforeach
+        </div>
+    </div>
 
-            <tbody>
-                @foreach ($days as $day)
-                    <tr>
-                        <th class="expire-mobile">{{$day->id}}</th>
-                        <td>
-                            <a href="{{ route('admin.days.show', ['day' => $day->id])  }}" style="color:white" class="ts bs a-notlink badge bg-primary rounded-pill"  > {{$day->day}} / {{$day->m}} / {{$day->y}}</a >
-                           
-                        </td>
+
+    {{-- <table class="table table-striped">
+
+        <tbody>
+            @foreach ($days as $day)
+                <tr>
+                    <th class="expire-mobile">{{$day->id}}</th>
+                    <td>
+                        <a href="{{ route('admin.days.show', ['day' => $day->id])  }}" style="color:white" class="ts bs a-notlink badge bg-primary rounded-pill"  > {{$day->day}} / {{$day->m}} / {{$day->y}}</a >
+                        
+                    </td>
+                
                     
-                       
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </tr>
+            @endforeach
+        </tbody>
+    </table> --}}
 
 
 @endsection
--- resources/views/admin/months/index.blade.php
+++ resources/views/admin/months/index.blade.php
@@ -3,24 +3,24 @@
 @section('contents')
 
     <?php 
-    // '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '16:30', '17:00', '17:30', '18:00', '18:30',
-    $times = [
-        1 => ['time' => '19:00', 'set' => ''] ,
-        2 => ['time' => '19:15', 'set' => ''] ,
-        3 => ['time' => '19:30', 'set' => ''] ,
-        4 => ['time' => '19:45', 'set' => ''] ,
-        5 => ['time' => '20:00', 'set' => ''] ,
-        6 => ['time' => '20:15', 'set' => ''] ,
-        7 => ['time' => '20:30', 'set' => ''] ,
-    ]; 
-    $days = [1, 2, 3, 4, 5, 6, 7];
-    $days_name = [' ','lunedì', 'martedi', 'mercoledì', 'giovedì', 'venerdì', 'sabato', 'domenica'];
-?>
+        // '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '16:30', '17:00', '17:30', '18:00', '18:30',
+        $times = [
+            1 => ['time' => '19:00', 'set' => ''] ,
+            2 => ['time' => '19:15', 'set' => ''] ,
+            3 => ['time' => '19:30', 'set' => ''] ,
+            4 => ['time' => '19:45', 'set' => ''] ,
+            5 => ['time' => '20:00', 'set' => ''] ,
+            6 => ['time' => '20:15', 'set' => ''] ,
+            7 => ['time' => '20:30', 'set' => ''] ,
+        ]; 
+        $days = [1, 2, 3, 4, 5, 6, 7];
+        $days_name = [' ','lunedì', 'martedi', 'mercoledì', 'giovedì', 'venerdì', 'sabato', 'domenica'];
+    ?>
 
-<div class="row">
-    <h1 >GESTIONE DATE</h1>
-    <a  href="{{ route('admin.setting') }}" class="btn btn-dark">INDIETRO</a>
-</div>
+    <div class="row">
+        <h1 >GESTIONE DATE</h1>
+        <a  href="{{ route('admin.setting') }}" class="btn btn-dark">INDIETRO</a>
+    </div>
 
     @if(session('success'))
         <div class="alert alert-success">
@@ -34,6 +34,15 @@
         </div>
     @endif
 
+    <h1 class="my-4">SCEGLI UN MESE</h1>    
+    <div class="container row gap-2 row-gap-3 py-5">
+        @foreach ($months as $month)
+            <a href="{{ route('admin.days.index', ['month' =>$month->n, 'year' =>$month->y ,'month_id' =>$month->id])  }}" class="col-auto shadow fw-semibold text-white a-notlink bg-primary px-4 py-1 rounded-5"  > {{$month->month}} {{$month->y}}</a >
+        @endforeach
+    </div>
+
+    <hr>
+
     {{-- Form per runnare il seeder --}}
     <form class="d-flex flex-column py-5"  action="{{ route('admin.dates.runSeeder') }}" method="post" enctype="multipart/form-data">
         @csrf
@@ -42,25 +51,25 @@
         <div class="input-group flex-nowrap py-2">
             <label for="max_reservations" class="input-group-text" >N° di posti a sedere</label>
             <input name="max_reservations" id="max_reservations" type="number" class="form-control" placeholder="N° di posti a sedere" aria-label="N° di posti a sedere" aria-describedby="addon-wrapping" value="0">
-          </div>
+        </div>
         <div>
         <h5 class="pt-4">Indica il numero massimo di pezzi al taglio per l'asporto</h5>
         <div class="input-group flex-nowrap py-2">
             <label for="max_pz_q" class="input-group-text" >N° di pezzi</label>
             <input name="max_pz_q" id="max_pz_q" type="number" class="form-control" placeholder="N° di pezzi" aria-label="N° di pezzi" aria-describedby="addon-wrapping" value="0">
-          </div>
+        </div>
         <div>
         <h5 class="pt-4">Indica il numero massimo di pizze al piatto per l'asporto</h5>
         <div class="input-group flex-nowrap py-2">
             <label for="max_pz_t" class="input-group-text" >N° di pizze</label>
             <input name="max_pz_t" id="max_pz_t" type="number" class="form-control" placeholder="N° di pezzi" aria-label="N° di pezzi" aria-describedby="addon-wrapping" value="0">
-          </div>
+        </div>
         <div>
         <h5 class="pt-4">Indica il numero massimo di pezzi per la consegna a domicilio</h5>
         <div class="input-group flex-nowrap py-2">
             <label for="max_domicilio" class="input-group-text" >N° di oridini a domicilio</label>
             <input name="max_domicilio" id="max_domicilio" type="number" class="form-control" placeholder="N° di pezzi" aria-label="N° di pezzi" aria-describedby="addon-wrapping" value="0">
-          </div>
+        </div>
         <div>
             <h5 class="pt-4">Seleziona i giorni in cui sei attivo</h5>
             <div class="btn-group py-1 row g-2 " role="group" aria-label="Basic checkbox toggle button group">
@@ -77,13 +86,11 @@
                                     id="days_off_{{ $day }}"
                                     name="days_off[]"
                                     value="{{ $day }}">
-                                 
+                                
                     
                                 
                                 <label class="btn btn-outline-dark" for="days_off_{{ $day }}">Attiva</label>
-{{--                                                            
-                                <label class="btn btn-outline-primary " for="days_off_{{ $day }}">Attiva</label>
-                                <input class="btn-check" type="checkbox" name="days_off[]" id="days_off_{{ $day }}" value="{{ $day }}"> --}}
+                                
                                 
                                 <h5 class="p-3">Seleziona le fasce orarie disponibili</h5>
                                 @foreach ($times as $time)                            
@@ -103,8 +110,8 @@
                             
                             </div>
                         </div>
-                       
-                   
+                    
+                
                     </label>
 
                 @endforeach
@@ -114,34 +121,6 @@
 
         <button class="btn btn-dark mt-4">Modifica</button>
     </form>
-
-        <hr>
-
-
-    <h1 class="my-4">SCEGLI UN MESE</h1>    
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                   
-                    <th>MESE</th>
-
-                   
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($months as $month)
-                    <tr>
-                        
-                        <td>
-                            <a href="{{ route('admin.days.index', ['month' =>$month->n, 'year' =>$month->y ,'month_id' =>$month->id])  }}" style="color:white" class="ts bs a-notlink badge bg-primary rounded-pill"  > {{$month->month}} / {{$month->y}}</a >
-                           
-                        </td>
-                    
-                       
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
 
     {{ $months->links() }}
 @endsection
