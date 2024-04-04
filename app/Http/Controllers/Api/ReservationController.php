f6f2d6566b959fb9d05ed686f1a720a36dac84b7
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-04-04 12:48:55 +0200

    df

--- app/Http/Controllers/Api/ReservationController.php
+++ app/Http/Controllers/Api/ReservationController.php
@@ -75,7 +75,7 @@ class ReservationController extends Controller
             Mail::to($data['email'])->send($mail);
 
             $mailAdmin = new confermaPrenotazioneAdmin($newOrder);
-            Mail::to('test@dashboardristorante.it')->send($mailAdmin);
+            Mail::to('info@pizzeria-capricciodileo.it')->send($mailAdmin);
 
 
             return response()->json([
